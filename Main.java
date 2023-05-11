29d8d02cbc2df57ec847c22b871970bd03a5069e
duongviethoang511 <92080405+duongviethoang511@users.noreply.github.com>
2023-05-11 09:56:47 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(2.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+        double circleArea = circle.getArea();
+        double circlePerimeter = circle.getPerimeter();
+        double rectangleArea = rectangle.getArea();
+        double rectanglePerimeter = rectangle.getPerimeter();
+
+        System.out.println("Circle Area: " + circleArea);
+        System.out.println("Circle Perimeter: " + circlePerimeter);
+        System.out.println("Rectangle Area: " + rectangleArea);
+        System.out.println("Rectangle Perimeter: " + rectanglePerimeter);
+    }
+}
